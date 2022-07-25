142561aa0c74c1d625aab27a4cd70e6715ae8bbe
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-26 05:18:18 +0800

    loading image

--- ImageEdit.py
+++ ImageEdit.py
@@ -3,4 +3,16 @@ Create the backend for the GUI.py to have functions that edits the images with t
 """
 
 # This import solves the import issues.
-import cv2
+import cv2 as cv
+import sys
+img = cv.imread(cv.samples.findFile("1.jpg"), cv.IMREAD_UNCHANGED)
+if img is None:
+    sys.exit("Image not exist")
+    
+#cv.namedWindow('My Image', cv.WINDOW_NORMAL)
+cv.imshow('My Image', img)
+
+k = cv.waitKey(0)
+cv.destroyAllWindows()
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
