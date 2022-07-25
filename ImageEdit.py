f34ae007121d1eff08667161d78698d42d879859
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-26 06:04:24 +0800

    image input and output
    image basic input and output with comments

--- ImageEdit.py
+++ ImageEdit.py
@@ -4,15 +4,24 @@ Create the backend for the GUI.py to have functions that edits the images with t
 
 # This import solves the import issues.
 import cv2 as cv
+import numpy
 import sys
+
+#read in the image data.
 img = cv.imread(cv.samples.findFile("1.jpg"), cv.IMREAD_UNCHANGED)
+
 if img is None:
     sys.exit("Image not exist")
-    
-#cv.namedWindow('My Image', cv.WINDOW_NORMAL)
+
+#set the window size to be controllable and fixed porpotion.    
+cv.namedWindow('My Image', cv.WINDOW_NORMAL)
+
+#display image
 cv.imshow('My Image', img)
 
-k = cv.waitKey(0)
+#Wait for a keystroke in the window
+cv.waitKey(0)
 cv.destroyAllWindows()
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+
+#write to output image file
+cv.imwrite("out.png", img);
