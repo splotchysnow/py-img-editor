0cb679ea873bd966fd9a3e7f61382ea0ec520412
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-07-25 22:40:46 -0700

    make more mode

--- linCirRec.py
+++ linCirRec.py
@@ -1,30 +1,35 @@
+from turtle import color
 import cv2
 import numpy as np
 
-
-drawing=False # true if mouse is pressed
-mode=True # if True, draw rectangle. Press 'm' to toggle to curve
-
+mode = 3
+# 1 for line
+# 2 for circle
+# 3 for rectangle
+drawing = False # true if mouse is pressed
+color = (100,100,100)
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
     global ix,iy,drawing, mode
-
     if event == cv2.EVENT_LBUTTONDOWN:
         ix,iy=x,y
-        drawing = True
     elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv2.circle(img,( int((x + ix)/2), int((y + iy)/2)), int(abs(x - ix)/2),(100,100,100), 3)        
-
-
+        if mode == 1: # drawing line
+            cv2.line(img,(ix, iy),(x,y),color, 3)
+        elif mode == 2: # drawing circle
+            cv2.circle(img,(int((x + ix)/2), int((y + iy)/2)), int(abs(x - ix)/2),color, 3)
+        else: #drawing rectangle
+            cv2.rectangle(img,(ix, iy),(x,y),color, 3)
+                    
+# the board to draw
 img = np.zeros((600,600,1), np.uint8)
 cv2.namedWindow('Window')
+# to connect to mouseCall and windows
 cv2.setMouseCallback('Window',draw_circle)
 while(1):
     cv2.imshow('Window',img)
     k = cv2.waitKey(1)&0xFF
-    if k == 27:
+    if k == 27: # ESE to quit
         break
 cv2.destroyAllWindows()
 
