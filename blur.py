9ec5fdc69add9f32830f9cd9c78975d2261f8370
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-07-26 08:54:42 +0800

    Update blur.py
    stuck in np array

--- blur.py
+++ blur.py
@@ -19,28 +19,56 @@ if img is None:
 #print(numpy.asarray(img))
 
 #save image to rewritable array 
-img_np = numpy.array(img)
+img_np = numpy.asarray(img)
 #create np array to store the 3d array of blurred image
-blur_np = numpy.zeros((img.shape))
+blur_np = img_np.copy()
 
 #print(len(blur_np)) 
 #print(len(blur_np[0])) 
 
 #loop through the numpy 3d array
+blur_np[0][0] = 1
+
+"""
 for x in range(0, len(blur_np)-1):
     for y in range(0, len(blur_np[x])-1):
         #average the upper, left, bottom, right pixels of original image and save to blur numpy array
         if (x>1 and y>1 and x<(len(blur_np)-2) and y<(len(blur_np[x])-2)):
-            print(x)
-            #for i in range(0,2):
-                #blur_np[x][y][i] = int(((int(img_np[x-1][y-1][i]))+(int(img_np[x-1][y+1][i]))+(int(img_np[x+1][y-1][i]))+(int(img_np[x+1][y+1][i])))/4)
+            #print(x)
+            for i in range(0,2):
+                blur_np[x][y][i] = int(((int(img_np[x-1][y-1][i]))+(int(img_np[x-1][y+1][i]))+(int(img_np[x+1][y-1][i]))+(int(img_np[x+1][y+1][i])))/4)
         else:
             #save the boundarys directly from original image
             blur_np[x][y] = img_np[x][y]
+"""
+"""
+for x in range(0, len(blur_np)-1):
+    for y in range(0, len(blur_np[x])-1):
+        #save the boundarys directly from original image
+        if x==0 or y==0 or x==len(blur_np-1) or y ==len(blur_np[x]-1):
+            blur_np[x][y] = img_np[x][y]
+        else:
+            #average the upper, left, bottom, right pixels of original image and save to blur numpy array
+            for i in range(0,2):
+                blur_np[x][y] = img_np[x][y]
+                #blur_np[x][y][i] = int(((int(img_np[x-1][y-1][i]))+(int(img_np[x-1][y+1][i]))+(int(img_np[x+1][y-1][i]))+(int(img_np[x+1][y+1][i])))/4)
+"""
+
+"""
+#print(blur_np[len(blur_np)-1][len(blur_np[0])-1][0])
+#blur_np[0][0][2] = img_np[0][0][1]
+#blur_np[len(blur_np)-1][len(blur_np[0])-1][0] = img_np[len(blur_np)-1][len(blur_np[0])-1][0]
+"""
+
+for x in range(0, len(img_np)-1):
+    for y in range(0, len(img_np[x])-1):
+        for i in range(0,2):
+            blur_np[x][y][i] = img_np[x][y][i]
+    
         
 
 #for debug usage, print 3d array of blurred image 
-print(blur_np)
+#print(blur_np)
 
 #Gaussian Blurring method testing
 blur = cv.GaussianBlur(img, (25,25),0)
