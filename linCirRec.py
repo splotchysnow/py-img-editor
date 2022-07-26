42a318516a17850e6fa19e1dbdb24af57ff4a88a
xiaoxia-xia <xiaoxiaxia88@gmail.com>
2022-07-25 22:45:06 -0700

    Update linCirRec.py

--- linCirRec.py
+++ linCirRec.py
@@ -10,9 +10,9 @@ drawing = False # true if mouse is pressed
 color = (100,100,100)
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing, mode
+    global ix, iy, mode
     if event == cv2.EVENT_LBUTTONDOWN:
-        ix,iy=x,y
+        ix,iy = x,y
     elif event == cv2.EVENT_LBUTTONUP:
         if mode == 1: # drawing line
             cv2.line(img,(ix, iy),(x,y),color, 3)
